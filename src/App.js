fadb02511ac5d53c4c33de6fd61f4933f972d413
Suryasnata Saha <suryasnata2303@gmail.com>
2021-11-07 17:47:19 +0530

    layout modal added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import HomePage from "./Pages/HomePage/HomePage";
 import AddExpense from "./Pages/AddExpense/AddExpense";
 import Navbar from "./core/Navbar";
 import { GlobalProvider } from "./context/ExpenseContext";
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
 
-function App() {
+function App({ open, onCloseModal }) {
   return (
     <GlobalProvider>
       <Router>
         <Navbar />
-
+        <Modal open={open} onClose={onCloseModal} center>
+          <h2>Simple centered modal</h2>
+        </Modal>
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/addexpense" component={AddExpense} />
--- src/core/Navbar.jsx
+++ src/core/Navbar.jsx
@@ -2,11 +2,13 @@ import React, { Fragment, useState, useContext } from "react";
 import DarkModeToggle from "react-dark-mode-toggle";
 import { ThemeContext } from "../context/ThemeContext";
 import { FcSettings } from "react-icons/fc";
-import ReactDOM from "react-dom";
 import "react-responsive-modal/styles.css";
 import { Modal } from "react-responsive-modal";
 const Navbar = () => {
   const [isDarkMode, setIsDarkMode] = useState(() => false);
+  const [open, setOpen] = useState(false);
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
   const theme = useContext(ThemeContext);
   const darkMode = theme.state.darkMode;
   const onClick = () => {
@@ -16,10 +18,6 @@ const Navbar = () => {
       theme.dispatch({ type: "DARKMODE" });
     }
   };
-  const [open, setOpen] = useState(false);
-
-  const onOpenModal = () => setOpen(true);
-  const onCloseModal = () => setOpen(false);
   return (
     <Fragment>
       <div className={`navbar ${darkMode ? "nav-dark" : "nav-light"}`}>
@@ -33,12 +31,28 @@ const Navbar = () => {
                 size={70}
               />
             </span>
-            <h1 className="settings" onClick={onOpenModal}>
+            <button className="settings" onClick={onOpenModal}>
               <FcSettings />
-            </h1>
+            </button>
           </div>
         </div>
       </div>
+      <Modal open={open} onClose={onCloseModal} center>
+        <div className="modal-wrapper">
+          <div className="layout">
+            <label for="layout-dropdown">Choose a layout: </label>
+            <select
+              name="layout-dropdown"
+              className="layout-dropdown"
+              placeholder="Select Layout"
+            >
+              <option value="budget">Budget App</option>
+              <option value="todo">Todo App</option>
+              <option value="fitness">Fitness Tracker App</option>
+            </select>
+          </div>
+        </div>
+      </Modal>
     </Fragment>
   );
 };
