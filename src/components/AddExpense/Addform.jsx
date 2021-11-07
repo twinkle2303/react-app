257e6518bd59ee651b374110eb1c5db4f272eb34
Suryasnata Saha <suryasnata2303@gmail.com>
2021-11-07 13:59:53 +0530

    theme toggle updated

--- src/components/AddExpense/Addform.jsx
+++ src/components/AddExpense/Addform.jsx
@@ -1,10 +1,13 @@
 import React, { Fragment, useContext, useState } from "react";
 import { GlobalContext } from "../../context/ExpenseContext";
 import { useHistory } from "react-router-dom";
+import { ThemeContext } from "../../context/ThemeContext";
 
 const Addform = () => {
   const history = useHistory();
   //Consumer
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
 
   let [formData, setFormData] = useState({
     description: "",
@@ -39,43 +42,51 @@ const Addform = () => {
 
   return (
     <Fragment>
-      <div className="form-heading">
+      <div
+        className={`form-heading ${darkMode ? "header-dark" : "header-light"}`}
+      >
         <div className="container">
           <h1>Add Expense</h1>
         </div>
       </div>
-      <div className="container form-wrapper">
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Description"
-            name="description"
-            onChange={handleChange}
-            value={formData.description}
-          />
-          <input
-            type="number"
-            placeholder="Amount"
-            name="amount"
-            onChange={handleChange}
-            value={formData.amount}
-          />
-          <input
-            type="date"
-            name="date"
-            onChange={handleChange}
-            value={formData.date}
-          />
-          <textarea
-            name="note"
-            cols="30"
-            rows="10"
-            placeholder="Add a note for your expensse (optional)"
-            onChange={handleChange}
-            value={formData.note}
-          ></textarea>
-          <input type="submit" value="Submit Form" />
-        </form>
+      <div className={`form-wrapper ${darkMode ? "wrap-dark" : "wrap-light"}`}>
+        <div className="container">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Description"
+              name="description"
+              onChange={handleChange}
+              value={formData.description}
+            />
+            <input
+              type="number"
+              placeholder="Amount"
+              name="amount"
+              onChange={handleChange}
+              value={formData.amount}
+            />
+            <input
+              type="date"
+              name="date"
+              onChange={handleChange}
+              value={formData.date}
+            />
+            <textarea
+              name="note"
+              cols="30"
+              rows="10"
+              placeholder="Add a note for your expensse (optional)"
+              onChange={handleChange}
+              value={formData.note}
+            ></textarea>
+            <input
+              type="submit"
+              value="Submit Form"
+              className={`submit-btn ${darkMode ? "btn-dark" : "btn-light"}`}
+            />
+          </form>
+        </div>
       </div>
     </Fragment>
   );
