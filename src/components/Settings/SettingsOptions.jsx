e912adf3258c1aa0907f303f43aaf0783211d8c0
Suryasnata Saha <suryasnata2303@gmail.com>
2021-11-14 13:14:39 +0530

    settings bugs fixed

--- src/components/Settings/SettingsOptions.jsx
+++ src/components/Settings/SettingsOptions.jsx
@@ -2,17 +2,8 @@ import React, { useContext } from "react";
 import { GlobalContext } from "../../context/ExpenseContext";
 
 const SettingsOptions = () => {
-  let { changetype } = useContext(GlobalContext);
+  let { changetype, settingstype } = useContext(GlobalContext);
   const handleClick = (e) => {
-    let ulchild = e.target.parentElement.children;
-    console.log(ulchild);
-    for (let i = 0; i < ulchild.length; i++) {
-      if (ulchild[i].classList.contains("clicked")) {
-        ulchild[i].classList.remove("clicked");
-      }
-    }
-    e.target.classList.add("clicked");
-
     changetype(e.target.textContent.trim());
   };
 
@@ -20,10 +11,20 @@ const SettingsOptions = () => {
     <div className="settingsoptions">
       <h2> Options</h2>
       <ul>
-        <li className="clicked options" onClick={(e) => handleClick(e)}>
+        <li
+          className={`options ${
+            settingstype === "Appearance" || settingstype === ""
+              ? "clicked"
+              : {}
+          }`}
+          onClick={(e) => handleClick(e)}
+        >
           Appearance
         </li>
-        <li className="options" onClick={(e) => handleClick(e)}>
+        <li
+          className={`options ${settingstype === "Layout" ? "clicked" : {}}`}
+          onClick={(e) => handleClick(e)}
+        >
           Layout
         </li>
       </ul>
